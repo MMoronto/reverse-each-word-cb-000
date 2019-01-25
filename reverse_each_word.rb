2676d82b6acbc408c7bca50833bbd3f05095e142
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-01-25 17:56:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
+sentence1 = "Hello there, and how are you?"
+
 def reverse_each_word(string)
-  reverse = []
-  string.each do |wordString|
-    
+  rever = string.split(" ").collect do |word|
+  word.reverse
   end
-end   
+  rever.join(" ")
+end 
